f2ff5515fac0510c9f796b705c5d78369b0e57e8
Pham Duc Hao <hao.pham1652003@gmail.com>
2022-08-26 19:22:20 +0700

    Change http to https

--- js/script.js
+++ js/script.js
@@ -13,15 +13,15 @@ $(function() {
 
 (function (global) {
     var dc = {};
-    var homeHtml = "snippets/home-snippet.html";
+    var homeHtml = "/snippets/home-snippet.html";
     var allCategoriesUrl =
-        "http://davids-restaurant.herokuapp.com/categories.json";
+        "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemUrl =
-        "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
-    var menuItemsTitleHtml = "snippets/menu-items-title.html";
-    var menuItemHtml = "snippets/menu-item.html";
+        "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    var menuItemsTitleHtml = "/snippets/menu-items-title.html";
+    var menuItemHtml = "/snippets/menu-item.html";
 
     var insertHtml = function (selector, html) {
         var targetElem = document.querySelector(selector);
